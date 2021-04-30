5fa25ad2f25c5fb38aa56a341353a69f5a37a96c
Alfredo Eleazar Orozco Quesada <ae.orozco10@uniandes.edu.co>
2021-04-29 23:09:42 -0500

    principios

---
+++ js/principles.js
@@ -0,0 +1,27 @@
+function cuidamos() {
+	document.getElementById('principio').innerHTML = "Nos importa lo que para las personas es importante. Procuramos apropiarnos de sus intereses y hacer transiciones significativas desde el momento inicial hasta el diseño final."
+}
+
+function interd() {
+	document.getElementById('principio').innerHTML = "La creación de nuevos objetos amerita entender los procesos desde todas sus dimensiones. La tecnología que se desarrolla debe ser coherente con esto."
+}
+
+function intuicion() {
+	document.getElementById('principio').innerHTML = "Diseñar requiere navegar entre las formas concretas y las intuiciones informales continuamente. Un/a diseñador/a debe sentirse cómodo/a modelando en ambos contextos."
+}
+
+function colaboracion() {
+	document.getElementById('principio').innerHTML = "Expandimos las posibilidades individuales mediante la colaboración. Hacemos preguntas entre nuestros equipos y a otras personas todo el tiempo. Disfrutamos colaborar con otras organizaciones y con las personas con quienes compartimos."
+}
+
+function divulga() {
+	document.getElementById('principio').innerHTML = "Los diseños no existen para ser abandonados en una isla, se crean para ser comunicados y utilizados entre las personas. Los diseños conectan a la gente porque tratan sobre lo que creemos y lo que somos."
+}
+
+function sostenible() {
+	document.getElementById('principio').innerHTML = "Tenemos hasta 2050 para reducir las emisiones de carbono a cero. Nuestros proyectos deben esforzarse por tener un impacto ambiental positivo o neutral. Además, nuestros diseños son financieramente autosostenibles."
+}
+
+function curioso() {
+	document.getElementById('principio').innerHTML = "Aprender es uno de los grandes placeres del diseño. Todo proyecto sigue una estrategia pedagógica por medio de Aprendizaje Basado en Proyectos para entrenar las competencias del equipo. Se adquieren progresivas responsabilidades también."
+}
