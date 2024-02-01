92e1f9c4975895d181687f9ea4077e17c5389c2f
MacBook <macbook@MacBook-Pro-de-MacBook.local>
2024-02-01 14:42:45 -0500

    Portafolio 2024

---
+++ portafolio/js/hover.js
@@ -0,0 +1,7 @@
+function show1() {
+  document.getElementById("slide1").style.display = "block";
+}
+
+function hide1() {
+  document.getElementById("slide1").style.display = "none";
+}
---
+++ portafolio/js/reload.js
@@ -0,0 +1,7 @@
+function recargar1(){
+	$('#proyecto-agencia').attr('src', $('#proyecto-agencia').attr('src'));
+}
+
+function recargar2(){
+	$('#proyecto-apata').attr('src', $('#proyecto-apata').attr('src'));
+}
