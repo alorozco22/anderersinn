7a38f6cdeeff57e8d553f2a798a253b39465b783
Alfredo Eleazar Orozco Quesada <ae.orozco10@uniandes.edu.co>
2024-08-08 04:09:54 -0500

    Update info

--- portafolios/js/reload.js
+++ portafolios/js/reload.js
@@ -1,7 +1,11 @@
 function recargar1(){
-	$('#proyecto-agencia').attr('src', $('#proyecto-agencia').attr('src'));
+	$('#proyecto-apata').attr('src', $('#proyecto-apata').attr('src'));
 }
 
 function recargar2(){
-	$('#proyecto-apata').attr('src', $('#proyecto-apata').attr('src'));
+	$('#proyecto-sofi').attr('src', $('#proyecto-sofi').attr('src'));
+}
+
+function recargar3(){
+	$('#proyecto-vitruvio').attr('src', $('#proyecto-vitruvio').attr('src'));
 }
